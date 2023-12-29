ddb8e4f3cf73cce0630b53936c105e15c23b2d43
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-29 20:09:02 +0530

    Adding the css

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from '@/styles/Root.module.css'
 
 export default function Home() {
     return (
         <main className="p-10 flex justify-around items-center">
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-6">
                 <h1 className="font-black text-5xl">What is Swap ai</h1>
                 <p className="text-lg">Swap ai is a generative ai tool to swap your images with your fantasy avatar</p>
-                <Link href="/Home" className="font-bold text-xl">
-                    Get started
+                <Link href="/Home" className={`font-bold text-xl max-w-max border-black border-2 py-3 px-10 rounded-lg transition ease-in-out ${styles.button} relative hover:text-black`}>
+                    <p>Get Started</p>
                 </Link>     
             </div>
             <div>
