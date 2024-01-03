7b3067610e31c1d5bfb6ed673dc25228e404b0c5
kanugurajesh <245122733172@mvsrec.edu.in>
2024-01-03 12:46:01 +0530

    Adding the replenished code

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/Contact", "/api/trigger"],
+  publicRoutes: ["/", "/Contact"],
 });
  
 export const config = {
