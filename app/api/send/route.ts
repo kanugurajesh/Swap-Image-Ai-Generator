39a2037cdad673f50d66e0a4530efaaa223883b9
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-23 20:58:48 +0530

    Adding fixes to email and styling

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,35 +1,24 @@
-import { NextRequest } from 'next/server';
-import { EmailTemplate } from '@/components/email-template';
-import { Resend } from 'resend';
+import { EmailTemplate } from "@/components/email-template";
+import { NextRequest } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
+  const reqBody = await request.json();
+  const { imageURl, email } = reqBody;
 
-    const body = await request.json();
+  try {
+    // @ts-ignore
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email as string,
+      subject: "Your avatar is ready! 🌟🤩",
+      react: EmailTemplate({ imageURl: imageURl }),
+    });
 
-    const { email, imageURl } = body;
-
-    if (!email) {
-        return Response.json({ error: 'Missing email' });
-
-    }
-    if (!imageURl) {
-        return Response.json({ error: 'Missing image' });
-    }
-
-    try {
-        
-        // @ts-ignore
-        const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: email,
-            subject: 'your image is ready',
-            react: EmailTemplate({ imageURl:imageURl }),
-        });
-
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
 }
